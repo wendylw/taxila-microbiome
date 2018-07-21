5328798e57b6a4a8459bc9ab5f375937e184f592
wendylin <wendylw2013@gmail.com>
2018-07-21 23:28:48 +0800

    dev: Update login desktop styles

---
+++ js/main.js
@@ -0,0 +1,13 @@
+(function(){
+  $('input[type="checkbox"]').on('click', function(e){
+    var isChecked = $(this).is(':checked');
+
+    if (isChecked) {
+      $(this).parent('.checkbox').addClass('active');
+    } else {
+      $(this).parent('.checkbox').removeClass('active');
+    }
+  });
+})();
+
+window.onload = function(){};
