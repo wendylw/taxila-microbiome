9c34d993224c3b61b14d4a94f7b2ba1dc48a0f6e
wendylin <wendylw2013@gmail.com>
2018-08-12 19:50:02 +0800

    dev: add alert and add-article

--- js/main.js
+++ js/main.js
@@ -8,4 +8,10 @@
       $(this).parent('.checkbox').removeClass('active');
     }
   });
+
+  $('body').on('click', '.news-info__dropdown-button', function(e) {
+    $(this).siblings('.news-info__sub-list').slideToggle();
+
+    e.stopPropagation();
+  });
 })();
